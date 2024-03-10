cf2c9867a8e079d9b8eafedf196efde950d889b6
Artem Hrabovskyi <artemygrab@gmail.com>
2024-03-10 11:24:51 +0200

    Update speech recognition

--- main.py
+++ main.py
@@ -73,22 +73,23 @@ async def record_and_transcribe(filename, segment_duration, total_duration, samp
         await asyncio.gather(record_task, transcribe_task)
 
         chat_completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": f'''Кілька прикладів особливостей жестової (візуальної) мови. Заперечення показують наприкінці речення: «Я не розумію» буде як «Я розумію ні». Спочатку кажуть про фон, а потім про решту, тобто жести Парта. Олівець. Під» означатиме «Олівець під партою.
+        messages=[
+            {
+                "role": "user",
+                 "content": f'''Кілька прикладів особливостей жестової (візуальної) мови. Заперечення показують наприкінці речення: «Я не розумію» буде як «Я розумію ні». Спочатку кажуть про фон, а потім про решту, тобто жести Парта. Олівець. Під» означатиме «Олівець під партою.
 
                     Якщо йдеться про якийсь об'єкт, ми кажемо «Він, його», але в розмові може виникнути плутанина «Учитель зустрів учня, і він його похвалив». У жестовій мові таких непорозумінь не виникає одна рука показуватиме, приміром, «учитель», а інша зробить низку жестів це розповідь про вчителя.
 
                     Перепиши наступну фразу так як би її сказали мовою жестів. Всі слова мають бути у базовій формі, не пиши ніяких пояснень, лише послідовність слів: {transcription}''',
-                }
-            ],
-            model="gpt-3.5-turbo",
-        )
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
 
         chat_response = chat_completion.choices[0].message.content
         chat_response_lower = chat_response.lower()
         list_chat_response = chat_response_lower.split()
+        print(list_chat_response)
         frameTime = 1
 
         while i < len(list_chat_response):
@@ -138,7 +139,7 @@ async def record_audio(filename, segment_duration, samplerate=44100):
 
 if __name__ == "__main__":
     audio_filename = "C:/Users/Oleksyi/Desktop/project/sounds/recorded_audio.wav"
-    segment_duration = 2
+    segment_duration = 3
     total_duration = 300
     start_time = time.time()
 
